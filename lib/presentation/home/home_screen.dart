fc3a3b50d9395acd069710dd73bf7f3b9969cf7d
san-smith <san-smith@mail.ru>
2020-02-29 23:58:29 +0700

    Update home screen

--- lib/presentation/home/home_screen.dart
+++ lib/presentation/home/home_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_bloc_arch/domain/bloc/user/users_bloc.dart';
+import 'package:flutter_bloc_arch/internal/dependencies/user/user_module.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key key}) : super(key: key);
@@ -8,36 +11,74 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
+  UsersBloc _usersBloc = UserModule.usersBloc();
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _usersBloc.add(UsersGetEvent());
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _usersBloc.close();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Пользователи'),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
+      body: _getBody(),
+    );
+  }
+
+  Widget _getBody() {
+    return BlocBuilder<UsersBloc, UsersState>(
+      bloc: _usersBloc,
+      builder: (context, state) {
+        return AnimatedSwitcher(
+          duration: Duration(milliseconds: 300),
+          child: Stack(
+            children: <Widget>[
+              if (state is UsersLoadingState) _getLoader(),
+              if (state is UsersFailedState) _getError(state),
+              if (state is UsersSuccessState) _getContent(state),
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  Widget _getLoader() {
+    return Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Widget _getError(UsersFailedState state) {
+    return Center(
+      child: Text('${state.error}'),
+    );
+  }
+
+  Widget _getContent(UsersSuccessState state) {
+    return ListView(
+      children: <Widget>[
+        ...state.users.map((item) => ListTile(
+              title: Text(
+                '${item.firstName} ${item.lastName}',
+                style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              subtitle: Text('${item.email}'),
+            )),
+      ],
     );
   }
 }
